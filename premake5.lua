dbaafa6210ccb9e240748dac077c2c085ddaebb3
zachbaker55 <111906222+zachbaker55@users.noreply.github.com>
2023-07-15 20:59:02 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -14,7 +14,12 @@ project "GLFW"
     "src/input.c",
     "src/monitor.c",
     "src/vulkan.c",
-    "src/window.c"
+    "src/window.c",
+    "src/platform.c",
+    "src/null_init.c",
+    "src/null_monitor.c",
+    "src/null_window.c",
+    "src/null_joystick.c"
   }
 
   filter "system:windows"
@@ -32,6 +37,7 @@ project "GLFW"
       "src/wgl_context.c",
       "src/egl_context.c",
       "src/osmesa_context.c",
+      "src/win32_module.c"
     }
 
     defines {
